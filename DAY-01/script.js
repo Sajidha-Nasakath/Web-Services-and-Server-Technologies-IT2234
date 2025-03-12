c0f57c3f83e93dc132b22194e80976e30e82bf9b
Sajidha-Nasakath <sajidhanasahath@gmail.com>
2025-03-12 20:21:07 +0530

    Created DAY-01 folder

---
+++ DAY-01/script.js
@@ -0,0 +1,63 @@
+console.log("Hello World!");
+
+let a = 35;
+let b = 80;
+
+let Addition = a + b;
+let Subraction = a - b;
+let Multiplication = a * b;
+let Division = a / b;
+
+console.log(Addition);
+console.log(Subraction);
+console.log(Multiplication);
+console.log(Division);
+
+var x = 20
+var x = 21
+var y = 52
+const z = 45
+
+console.log(x + y);
+console.log(z);
+
+//print numbers from 1 to 10
+console.log("print numbers from 1 to 10")
+for(let r=1; r<=10; r++){
+	console.log(r);
+}
+
+//print only the odd numbers
+console.log("print only the odd numbers")
+for(let s=1; s<=10; s++){
+	if(s%2 == 1){
+		console.log(s);
+	}
+}
+
+//print the numbers in reverse
+console.log("print the numbers in reverse")
+for(let t=10; t>=1; t--){
+		console.log(t);
+}
+
+
+console.log("reverse the numbers in left and right 4321-5-9876")
+
+console.log("")
+ 
+let num=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+  
+    let mid=(num.length-1)/2
+    
+    for(let h=mid-1; h>=0; h--)
+    {
+ 	   console.log(num[h])
+    }
+ 	console.log(num[mid])
+ 	
+ 	for(let h=num.length; h>mid; h--)
+ 	{
+ 		console.log(num[h])
+ 	}
+
